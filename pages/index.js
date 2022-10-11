d6e20e0788d22a3adb3af23ccfa02e9ee788e285
Jeff Passetti <jpassetti@gmail.com>
2022-10-11 15:01:23 -0400

    the alexia in the head section

--- pages/index.js
+++ pages/index.js
@@ -24,11 +24,11 @@ export async function getStaticProps() {
 const Home = ({people, sponsors}) => {
 	return <Layout>
 		<Head>
-			<title>The Fall Workshop | Newhouse School at Syracuse University</title>
+			<title>The Alexia Fall Workshop | Newhouse School at Syracuse University</title>
 			<meta charSet="UTF-8" />
 			<meta property="og:type" content="website" />
         	<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-			<meta property="og:title" content="The Fall Workshop | Newhouse School at Syracuse University" key="title" />
+			<meta property="og:title" content="The Alexia Fall Workshop | Newhouse School at Syracuse University" key="title" />
 			<meta name="description" content="Founded by Visual Communications faculty in 1999, the workshop brings top professionals from around the world to join our professors as we push students to identify, observe and artfully communicate the core of who we are and the issues we face in everyday life." />
 		</Head>
 		<About />
