dce2804a70976461079f608bc02484c39fc0d206
Jeff Passetti <jpassetti@gmail.com>
2022-10-16 12:32:19 -0400

    nikon logo updated

--- components/Col.js
+++ components/Col.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import classNames from 'classnames/bind';
 import * as styles from './col.module.scss';
 
--- components/Sponsors.js
+++ components/Sponsors.js
@@ -5,19 +5,27 @@ import Row from './Row'
 import Section from './Section'
 import Sponsor from './Sponsor'
 
+
+import classNames from 'classnames/bind';
 import styles from './sponsor.module.scss'
 
-const Sponsors = ({sponsors}) => {
+let cx = classNames.bind(styles);
 
+
+const Sponsors = ({sponsors}) => {
+	let nikonClasses = cx({
+		sponsor : true,
+		nikon : true
+	});
 	return <Section id="sponsors" title="Sponsors" backgroundColor="orange_10">
 		<Container size="sm">
 			{/* NIKON 458.6 400 */ }
 			<Row justifyContent="center">
 				<Col xs="10" sm="8" md="5">
-					<div className={styles.sponsor}>
+					<div className={nikonClasses}>
 					<a href="https://www.nikonusa.com/" target="_blank">
 						<Image
-								src="/sponsors/nikonusa-logo-color.svg"
+								src="/sponsors/38mm_NikonBSgrad_4c_bk.png"
 								alt="Nikon USA logo"
 								width={458.6}
 								height={400}
