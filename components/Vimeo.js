ab9d899ea45581eb6f5aa2170a67dddfcad93c4d
Jeff Passetti <jpassetti@Jeffs-MacBook-Air.local>
2022-12-03 10:38:18 -0500

    vimeo src conditional

--- components/Vimeo.js
+++ components/Vimeo.js
@@ -4,6 +4,7 @@ import styles from './vimeo.module.scss';
 let cx = classNames.bind(styles);
 
 const Vimeo = ({src}) => {
+	console.log({src});
 	const vimeoClasses = cx({
 		[`embed-responsive`] : true,
 		[`embed-responsive-16by9`] : true
@@ -11,8 +12,8 @@ const Vimeo = ({src}) => {
 	const iframeClasses = cx({
 		[`embed-responsive-item`] : true,
 	})
-	const vimeoId = src.substring(src.length - 9);
+	const vimeoId = src?.substring(src.length - 9);
 
-	return <div className={vimeoClasses}><iframe className={iframeClasses} title="The Fall Workshop 2019" src={`https://player.vimeo.com/video/${vimeoId}`} width="500" height="281" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen="" data-ready="true"></iframe></div>
+	return src ? <div className={vimeoClasses}><iframe className={iframeClasses} title="The Fall Workshop 2019" src={`https://player.vimeo.com/video/${vimeoId}`} width="500" height="281" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen="" data-ready="true"></iframe></div>: '';
 }
 export default Vimeo;
