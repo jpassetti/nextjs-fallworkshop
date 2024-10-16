38bd97c84adf8621cc97bfe7e92216ca4ddadb01
Jeff Passetti <jpassetti@gmail.com>
2024-10-16 09:30:20 -0400

    footer copyright

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,39 @@
-import Container from './Container'
-import Logo from './Logo'
-import Paragraph from './Paragraph'
+import Container from "./Container";
+import Logo from "./Logo";
+import Paragraph from "./Paragraph";
 
-import styles from './footer.module.scss'
+import styles from "./footer.module.scss";
 
 const Footer = () => {
-	return <footer className={styles.footer}>
-		<Container>
-			<Paragraph color="white" marginBottom="4">Copyright 2023-24.<br />
-			Newhouse School at Syracuse University.</Paragraph>
-			<Paragraph color="white" marginBottom="4">
-				<a style={{color: "white", margin: "1rem"}} target="_blank" href="https://www.syracuse.edu/life/accessibility-diversity/accessible-syracuse/">Accessibility</a> 
-				<a style={{color: "white", margin: "1rem"}} target="_blank" href="https://www.syracuse.edu/about/site/privacy-policy/">Privacy</a></Paragraph>
-			<a href="https://newhouse.syr.edu" target="_blank"><Logo invert /></a>
-		</Container>
-	</footer>
-}
+ return (
+  <footer className={styles.footer}>
+   <Container>
+    <a href="https://newhouse.syr.edu" target="_blank">
+     <Logo invert />
+    </a>
+    <Paragraph color="white" marginBottom="4">
+     Copyright {new Date().getFullYear()}
+     <br />
+     Newhouse School at Syracuse University.
+    </Paragraph>
+    <Paragraph color="white" marginBottom="4">
+     <a
+      style={{ color: "white", margin: "1rem" }}
+      target="_blank"
+      href="https://www.syracuse.edu/life/accessibility-diversity/accessible-syracuse/"
+     >
+      Accessibility
+     </a>
+     <a
+      style={{ color: "white", margin: "1rem" }}
+      target="_blank"
+      href="https://www.syracuse.edu/about/site/privacy-policy/"
+     >
+      Privacy
+     </a>
+    </Paragraph>
+   </Container>
+  </footer>
+ );
+};
 export default Footer;
