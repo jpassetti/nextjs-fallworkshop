07df43d0ba9c75cd954ec1586ee975817ff6b270
Jeff Passetti <jpassetti@gmail.com>
2024-10-16 10:10:22 -0400

    faculty staff cleanup

--- components/SocialMediaLinks.js
+++ components/SocialMediaLinks.js
@@ -1,41 +1,60 @@
-import Icon from './Icon'
+import Icon from "./Icon";
 
-import classNames from 'classnames/bind'
-import styles from './socialmedialinks.module.scss'
+import classNames from "classnames/bind";
+import styles from "./socialmedialinks.module.scss";
 
 let cx = classNames.bind(styles);
 
-
-const SocialMediaLinks = ({linksInformation, color}) => {
-	const {linkedinUrl, instagramUrl, twitterUrl, websiteUrl, links} = linksInformation;
-	let socialLinksAnchorClasses = cx({
-		socialLinksAnchor: true,
-		[`color-${color}`] : color
-	});
-	const getCorrespondingUrl = (link) => {
-		switch (link) {
-			case 'linkedin':
-				return linkedinUrl;
-				break;
-			case 'instagram':
-				return instagramUrl;
-				break;
-			case 'twitter':
-				return twitterUrl;
-				break;
-			case 'website':
-				return websiteUrl;
-				break;
-			}
-	}
-	return <ul className={styles.socialLinksList}>
-		{links.map((link, index)=> {
-			return <li key={index} className={styles.socialLinksListItem}>
-				<a target="_blank" href={getCorrespondingUrl(link)} className={socialLinksAnchorClasses}>
-					<Icon icon={link} />
-				</a>
-			</li>
-		})}
-	</ul>
-}
-export default SocialMediaLinks
+const SocialMediaLinks = ({ linksInformation, color }) => {
+ //console.log({ linksInformation });
+ const {
+  linkedinUrl,
+  instagramUrl,
+  twitterUrl,
+  websiteUrl,
+  facultyBioUrl,
+  links,
+ } = linksInformation;
+ let socialLinksAnchorClasses = cx({
+  socialLinksAnchor: true,
+  [`color-${color}`]: color,
+ });
+ const getCorrespondingUrl = (link) => {
+  switch (link) {
+   case "linkedin":
+    return linkedinUrl;
+    break;
+   case "instagram":
+    return instagramUrl;
+    break;
+   case "twitter":
+    return twitterUrl;
+    break;
+   case "website":
+    return websiteUrl;
+    break;
+   case "facultyBio":
+    return facultyBioUrl;
+    break;
+  }
+ };
+ return (
+  <ul className={styles.socialLinksList}>
+   {links.map((link, index) => {
+    // console.log({ link });
+    return (
+     <li key={index} className={styles.socialLinksListItem}>
+      <a
+       target="_blank"
+       href={getCorrespondingUrl(link)}
+       className={socialLinksAnchorClasses}
+      >
+       <Icon icon={link} />
+      </a>
+     </li>
+    );
+   })}
+  </ul>
+ );
+};
+export default SocialMediaLinks;
